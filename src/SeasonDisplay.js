abcfdc30d23450134179d86b4412d272ac6a7a4b
Sokos6 <will.sokolowski@gmail.com>
2020-09-05 18:56:12 -0500

    working season function

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = (props) => {
   console.log(props.lat);
-  return <div>Season Display</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  return <div>
+    Season Display
+    <div>{season}</div>
+
+    </div>
+
 };
 
 export default SeasonDisplay;
