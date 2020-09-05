e19c95fc535336947f7c20290451e3bb446f4db2
Sokos6 <will.sokolowski@gmail.com>
2020-09-04 20:34:05 -0500

    latitude set in state

--- src/index.js
+++ src/index.js
@@ -12,15 +12,20 @@ import ReactDOM from 'react-dom';
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = { lat: null };
-    console.log(this.state);
-  }
-  render() {
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
       (err) => console.log(err)
     );
-    return <div>Latitude:</div>;
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
