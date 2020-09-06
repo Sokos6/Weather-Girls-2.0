5a8910cdd8a63c483890b6105d7df51fb0048747
Sokos6 <will.sokolowski@gmail.com>
2020-09-05 19:08:28 -0500

    working icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,7 +12,15 @@ const SeasonDisplay = (props) => {
   console.log(props.lat);
   const season = getSeason(props.lat, new Date().getMonth());
   const text = season === 'winter' ? 'Stay warm!' : 'Be Sure to Hydrate!';
-  return <div>{text}</div>;
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
